00b283dfafbd21fc60f9361fce0f2fbbd5ee505e
Fouad Tabbara <fmtabbara@hotmail.co.uk>
2018-12-02 21:00:47 +0000

    Set up connection to local db with cloud db as fallback

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,11 +2,30 @@ const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
-mongoose.connect(
-  'mongodb://Fouad:todolist1@ds141284.mlab.com:41284/todolist',
-  { useNewUrlParser: true },
-  () => console.log('Connected to the Database!'),
-  err => console.log('Couldnt connect to the database...')
-);
+mongoose
+  .connect(
+    'mongodb://localhost:27017/TodoApp',
+    { useNewUrlParser: true }
+  )
+  .then(
+    () => {
+      console.log('Connected to the Local Database!');
+    },
+    err => {
+      mongoose
+        .connect(
+          'mongodb://Fouad:todolist1@ds141284.mlab.com:41284/todolist',
+          { useNewUrlParser: true }
+        )
+        .then(
+          () => {
+            console.log('Connected to the Cloud Database!');
+          },
+          err => {
+            console.log('Couldnt connect to the database..');
+          }
+        );
+    }
+  );
 
 module.exports = { mongoose };
