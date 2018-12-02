e9a3855c2fb981801cdfe5d069d1888d43f4cfe1
Fouad Tabbara <fmtabbara@hotmail.co.uk>
2018-12-02 22:00:06 +0000

    Added playground file mongoose-remove

---
+++ playground/mongoose-remove.js
@@ -0,0 +1,12 @@
+const { mogoose } = require('./../server/db/mongoose');
+const { Todo } = require('./../server/models/todo');
+const { User } = require('./../server/models/User');
+const { ObjectID } = require('mongodb');
+
+// Todo.remove({}).then(res => {
+//   console.log(res)
+// })
+
+Todo.findByIdAndRemove('5c02f38bc5bd9918b43adf18').then(res => {
+  console.log(res);
+});
